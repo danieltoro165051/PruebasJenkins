40adddaac9f0d01dc65a060bce7467f7f25e3872
Daniel Toro <danieltoro11@hotmail.com>
2020-04-28 10:06:08 -0500

    Se modifica ruta de .pem

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -7,6 +7,6 @@ echo $BUILD_TAG >> /tmp/.auth
 
 
 # Transifere el archivo
-scp  -i /home/usuario/EntrenamientoDevOps/jenkins/ec2/aws-devops.pem /tmp/.auth  ubuntu@ec2-52-39-179-208.us-west-2.compute.amazonaws.com:/tmp/.auth
-scp  -i /home/usuario/EntrenamientoDevOps/jenkins/ec2/aws-devops.pem /home/usuario/EntrenamientoDevOps/jenkins/pipeline/jenkins/deploy/publish ubuntu@ec2-52-39-179-208.us-west-2.compute.amazonaws.com:/tmp/publish
-ssh -i /home/usuario/EntrenamientoDevOps/jenkins/ec2/aws-devops.pem  ubuntu@ec2-52-39-179-208.us-west-2.compute.amazonaws.com /tmp/publish
+scp  -i /opt/aws-devops.pem /tmp/.auth  ubuntu@ec2-52-39-179-208.us-west-2.compute.amazonaws.com:/tmp/.auth
+scp  -i /opt/aws-devops.pem /home/usuario/EntrenamientoDevOps/jenkins/pipeline/jenkins/deploy/publish ubuntu@ec2-52-39-179-208.us-west-2.compute.amazonaws.com:/tmp/publish
+ssh -i /opt/aws-devops.pem  ubuntu@ec2-52-39-179-208.us-west-2.compute.amazonaws.com /tmp/publish
