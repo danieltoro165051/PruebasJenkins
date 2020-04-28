db3953dbf050d1e50cb89719b407e11b849ad100
Daniel Toro <danieltoro11@hotmail.com>
2020-04-28 10:20:35 -0500

    Se ajustan las rutas de ejecuciones

--- jenkins/test/test.sh
+++ jenkins/test/test.sh
@@ -4,5 +4,5 @@ echo "################"
 echo "*** Testing  ***"
 echo "################"
 
-docker run --rm -v /root/.m2:/root/.m2 -v /home/ricardo/jenkins/jenkins_home/workspace/pipeline-docker-maven/java-app:/app -w /app maven:3-alpine "$@"
+docker run --rm -v /root/.m2:/root/.m2 -v /home/usuario/EntrenamientoDevOps/jenkins/jenkins_home/workspace/pipeline-docker-maven/java-app:/app -w /app maven:3-alpine "$@"
 
